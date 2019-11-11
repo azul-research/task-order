9623eb95788f50e6cb3409092eb5a92177ff3b26
andrewprudnikov23.gmail.com <andrewprudnikov23@gmail.com>
2019-11-11 16:22:09 +0300

    change according to PR notes

--- generate_input.cpp
+++ generate_input.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
+#include <random>
 #include <string>
-#include <time.h>
+#include <chrono>
 
 int main() {
 	using namespace std;
-	srand(time(NULL));
 	freopen("input.txt", "w", stdout);
-	int Time = 100;
-	int duration = 20;
-	int n_workers = 1 + rand() % 100;
-	int n_managers = 1 + rand() % 4;
-	int n_types = 5 + rand() % 5;
-	cout << n_types << "\n";
-	for (int i = 0; i < n_types; ++i) {
-		cout << i << " " << 1 + rand() % (Time / 2) << "\n";
+	std::default_random_engine g;
+	g.seed(std::chrono::system_clock::now().time_since_epoch().count());
+	int dur = 20;
+	int min_t = 1;
+	int max_t = 100;
+	int max_w = 100;
+	int min_ww = 1;
+	int max_ww = 100;
+	int max_m = 4;
+	int min_tt = 5;
+	int max_tt = 9;
+	int min_tc = 1;
+	int max_tc = max_t / 2;
+	std::uniform_int_distribution<int> wd(1, max_w);
+	std::uniform_int_distribution<int> wtid(min_t, max_t);
+	std::uniform_int_distribution<int> wtod(min_t, dur);
+	std::uniform_int_distribution<int> wwd(min_ww, max_ww);
+	std::uniform_int_distribution<int> md(1, max_m);
+	std::uniform_int_distribution<int> td(min_tt, max_tt);
+	std::uniform_int_distribution<int> tcd(min_tc, max_tc);
+	int w = wd(g);
+	int m = md(g);
+	int t = td(g);
+	cout << t << "\n";
+	for (int i = 0; i < t; ++i) {
+		cout << i << " " << tcd(g) << "\n";
 	}
-	cout << "\n" << n_managers << "\n";
-	for (int i = 0; i < n_managers; ++i) {
+	std::uniform_int_distribution<int> wtd(0, t - 1);
+	cout << "\n" << m << "\n";
+	for (int i = 0; i < m; ++i) {
 		cout << 0 << " " << i << "\n";
 	}
-	cout << "\n" << n_workers << "\n" ;
-	int t_in;
-	for (int i = 0; i < n_workers; ++i) {
-		t_in = 1 + rand() % (Time - 1);
-		cout << t_in << " " << min(Time, t_in + 1 + rand() % duration) << " " << rand() % n_types << " " << rand() % 100 << " " << i << "\n";
+	cout << "\n" << w << "\n" ;
+	int ti;
+	for (int i = 0; i < w; ++i) {
+		ti = wtid(g);
+		cout << ti << " " << min(max_t, ti + wtod(g)) << " " << wtd(g) << " " << wwd(g) << " " << i << "\n";
 	}
 	return 0;
 }
