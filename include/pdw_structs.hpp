63cfcaa477b1edc758914ee03a80fc7d91568662
Ekaterina Gashchenko <katyagas@gmail.com>
2019-11-15 20:12:57 +0300

    delete _Tasks from calculate_d

--- include/pdw_structs.hpp
+++ include/pdw_structs.hpp
@@ -2,6 +2,8 @@
 
 typedef unsigned long ulong;
 
+using namespace std;
+
 struct pdw_task {
     ulong d;
     ulong p;
@@ -17,24 +19,24 @@ struct pdw_worker {
     ulong start;
     ulong finish;
 
+    pdw_worker();
     pdw_worker(ulong w, ulong type, ulong start, ulong finish);
 };
 
 class calculate_d {
     vector<pdw_worker> _Workers;
-    vector<pdw_task> _Tasks;
 
-    ulong find_first_start(int i);
-    ulong find_first_end(int i);
-    ulong find_last_start(int i);
-    ulong find_last_end(int i);
-    ulong find_middle(int i);
+    ulong find_first_start(size_t id);
+    ulong find_first_end(size_t id);
+    ulong find_last_start(size_t id);
+    ulong find_last_end(size_t id);
+    ulong find_middle(size_t id);
 
 public:
     enum type_d {
         FS, FE, LS, LE, M
     };
 
-    calculate_d(vector<pdw_worker> &Workers, vector<pdw_task> &Tasks);
-    ulong find_d(type_d type, int i);
+    calculate_d(vector<pdw_worker> &Workers);
+    ulong find_d(size_t id, type_d type);
 };
