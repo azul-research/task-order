ac1e0b65da1b7e5deecff3b6f36138d8307d6f67
andrewprudnikov23.gmail.com <andrewprudnikov23@gmail.com>
2019-12-02 12:17:03 +0300

    fix main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include "company.h"
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream>
+#include "ls.h"
+#include "evr_1.h"
+#include "edging.h"
+#include "GT_algorithm.hpp"
 #include "sum_wT.h"
 
 int main(int argc, char **argv) {
+	if (argc == 1) {
+	    std::cout << "*.exe input_file output_file algorithm\nAlgorithms: naive, sum_wT, GT, edging, evr_1\n";
+		return 0;
+	}
+	std::ifstream file(argv[1]);
+	std::cin.rdbuf(file.rdbuf());
+	std::ofstream cout(argv[2]);
 	unsigned long  m_size, t_size, w_size;
 	std::vector<Task> Tasks;
 	std::vector<Manager> Managers;
@@ -26,16 +39,31 @@ int main(int argc, char **argv) {
 		std::cin >> w;
 		Workers.push_back(w);
 	}
-	if (argc > 1) {
-		if (argv[1] == (char *)"naive") {
-			Result result = Result();
-			result.calculate_minimum(Tasks, Managers, Workers);
-			std::cout << result.get_value() << "\n";
-		}
-		if (argv[1] == (char *)"sum_wT") {
-			sum_wT_algorithm wT = sum_wT_algorithm(Workers, Tasks, type_d::LAST_START);
-			wT.calculate_result();
-		}
+	std::string algo = argv[3];
+	if (!algo.compare("naive")) {
+		Result result = Result();
+		result.calculate_minimum(Tasks, Managers, Workers);
+		cout << result.get_value() << "\n";
+	}
+	if (!algo.compare("sum_wT")) {
+		sum_wT_algorithm wT = sum_wT_algorithm(Workers, Tasks, type_d::LAST_START);
+		wT.calculate_result();
+		cout << WT.result << "\n";
+	}
+	if (!algo.compare("GT")) {
+		GT_algorithm GT = GT_algorithm(Workers, Tasks, type_d::LAST_START);
+		GT.calculate_result();
+		cout << GT.result << "\n";
+	}
+	if (!algo.compare("edging")) {
+		edging_algorithm ea = edging_algorithm(Workers, Tasks, type_d::LAST_START);
+		ea.calculate();
+		cout << ea.result << "\n";
+	}
+	if (!algo.compare("evr_1")) {
+		evr_1 e1 = evr_1();
+		e1.calculate_result(Tasks, Managers, Workers);
+		cout << e1.result << "\n";
 	}
 	return 0;
 }
